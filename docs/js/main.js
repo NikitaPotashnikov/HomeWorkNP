0535d31522aca3e6b0626ae8543b9788cd8fe5b4
NikitaPotashnikov <125442827+NikitaPotashnikov@users.noreply.github.com>
2023-04-02 14:10:17 +0300

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,58 @@
+$(function () {
+  
+
+  $('.dropdown-btn').on('click', function (e) {
+    e.preventDefault()
+    $(this).next().slideToggle('')
+  })
+  
+  $('.faq__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('faq__acc-link--active')) {
+      $(this).removeClass('faq__acc-link--active')
+      $(this).children('.faq__acc-text').slideUp()
+    } else {
+      $('.faq__acc-link').removeClass('faq__acc-link--active')
+      $('.faq__acc-text').slideUp()
+      $(this).addClass('faq__acc-link--active')
+      $(this).children('.faq__acc-text').slideDown()
+    }
+  })
+  
+  $('.launch__app, .launch__close, .overlay__launch').on('click', function (e) {
+    e.preventDefault()
+    $('.popup__launch').toggleClass('open')
+    $('body').toggleClass('no__scrol')
+  })
+
+  $('.connect__country, .connection__close, .overlay__connection').on('click', function (e) {
+    e.preventDefault()
+    $('.popup__connection').toggleClass('open')
+    $('body').toggleClass('no__scrol')
+  })
+
+  $('.connection__button').on('click', function(e){
+    e.preventDefault()
+    $('.popup__connection').removeClass('open')
+    $('.popup__confirmation').toggleClass('open')
+  })
+
+  $('.confirmation__close, .overlay__confirmation, .confirmation__link').on('click', function (e) {
+    e.preventDefault()
+    $('.popup__confirmation').toggleClass('open')
+    $('body').removeClass('no__scrol')
+  })
+  $('.burger, .overlay').on('click', function (e) {
+    e.preventDefault()
+    $('.header__nav ').toggleClass('header__nav--open')
+    $('.overlay').toggleClass('overlay--show')
+  })
+
+  $('.header__nav a').on('click', function (e) {
+    if ($(window).width() <= 320) {
+      $('.header__nav ').toggleClass('header__nav--open')
+      $('.overlay').toggleClass('overlay--show')
+    }
+  })
+
+})
